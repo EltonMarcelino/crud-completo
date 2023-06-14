4e21860e8fb606359188cb27b26db85f3b90564f
Elton marcelino <eltonfirminomarcelino@gmail.com>
2023-06-14 13:59:00 -0300

    criando a estrutura do projeto

---
+++ index.js
@@ -0,0 +1,27 @@
+let main = document.getElementsByTagName("main")
+let listViw = document.createElement("ul")
+
+let task = document.createElement("li")
+
+let input = document.createElement("input")
+
+let send = document.createElement("submit")
+
+let  deleted = document.createElement("submit")
+
+
+function readList(){
+
+}
+
+function createList(){
+
+}
+
+function updateList(){
+
+}
+
+function delet(){
+
+}
