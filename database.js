d7dbabe4d83410993fa4ba7eb61569c337be619f
Elton marcelino <eltonfirminomarcelino@gmail.com>
2023-06-15 08:31:10 -0300

    criando crud

---
+++ database.js
@@ -0,0 +1,10 @@
+export const tarefas = [
+    {"id":1,
+    "tarefa":"limpar casa"
+
+    },
+    {"id":2,
+    "tarefa":"drobrar roupa"
+
+    }
+]
--- index.js
+++ index.js
@@ -1,27 +1,112 @@
-let main = document.getElementsByTagName("main")
-let listViw = document.createElement("ul")
+import { tarefas } from "./database.js";
 
-let task = document.createElement("li")
+function cardTarefa(element) {
+  const deleted = document.createElement("button");
+  const atualizar = document.createElement("button");
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  const inputAtualizar = document.createElement("input");
 
-let input = document.createElement("input")
+  deleted.innerText = "Excluir";
+  deleted.dataset.id = element.id;
+  deleted.classList.add("delete__button");
 
-let send = document.createElement("submit")
+  atualizar.innerText = "Atualizar";
+  atualizar.dataset.id = element.id;
+  atualizar.classList.add("atualizar__button");
 
-let  deleted = document.createElement("submit")
+  inputAtualizar.placeholder = "Novo value";
+  inputAtualizar.dataset.id = element.id;
+  inputAtualizar.classList.add("input_atualizar");
 
+  p.innerText = element.tarefa;
+  p.classList.add('paragrafo')
+  li.classList.add("list__tarefas");
 
-function readList(){
-
+  li.appendChild(p);
+  li.appendChild(deleted);
+  li.appendChild(atualizar);
+  li.appendChild(inputAtualizar);
+  return li;
 }
 
-function createList(){
+function renderizar(array) {
+  const lista = document.querySelector("#lista");
+  lista.innerHTML = "";
+  array.forEach((element) => {
+    const tarefa = cardTarefa(element);
+    lista.appendChild(tarefa);
+  });
+  deleteTarefa(array);
+  atualizarTarefa(array);
+}
+renderizar(tarefas);
 
+function deleteTarefa(array) {
+  const deletar = document.querySelectorAll(".delete__button");
+
+  deletar.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const id = event.target.dataset.id;
+      const findTarefa = array.findIndex((tarefa) => tarefa.id === Number(id));
+      const removeItem = array.splice(findTarefa, 1);
+      renderizar(array);
+    });
+  });
 }
 
-function updateList(){
+function addTarefa(array) {
+  const input = document.querySelector("#input");
+  const buttonAdd = document.querySelector("#add");
+  input.classList.add("add__input");
+  input.name = "tarefa";
+  let newarray = {};
+  let count = 0;
+  buttonAdd.addEventListener("click", (event) => {
+    event.preventDefault();
 
+    newarray.id = array.length + 1;
+    newarray.tarefa = input.value;
+    if ((input.value = "")) {
+      count++;
+    }
+    if (count !== 0) {
+      count = 0;
+      return alert("preencha o campo");
+    }
+    array.push(newarray);
+    newarray = {};
+    renderizar(array);
+  });
 }
+addTarefa(tarefas);
 
-function delet(){
+function atualizarTarefa(array) {
+  const input = document.querySelectorAll(".input_atualizar");
+  const atualizar = document.querySelectorAll(".atualizar__button");
+  const p = document.querySelectorAll('.paragrafo');
 
+  const atualizarLista = {};
+  let count = 0;
+  atualizar.forEach(button => {
+    button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const id = event.target.dataset.id;
+    alert(id)
+    atualizarLista.id = array.id;
+    atualizarLista.tarefa = input.value;
+    const findTarefa = array.findIndex((tarefa) => tarefa.id === Number(id));
+    if ((input.value = "")) {
+      count++;
+    }
+    if (count !== 0) {
+      count = 0;
+      return alert("preencha o campo");
+    }
+    array.slice(findTarefa);
+    renderizar(array);
+  });
+  })
+  
+  
 }
